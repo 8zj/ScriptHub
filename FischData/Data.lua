6c33c3a9937914aa122d91b1003d11348c419699
Pick <90311538+8zj@users.noreply.github.com>
2025-04-26 23:15:43 -0400

    Update Data.lua

--- FischData/Data.lua
+++ FischData/Data.lua
@@ -1,4 +1,11 @@
 return {
-    "LEGO",
-    "LEGO - Studolodon"
+    NormalZones = {
+        "Zone 1",
+        "Zone 2",
+        "Zone 3"
+    },
+    EventZones = {
+        "LEGO",
+        "LEGO - Studolodon"
+    }
 }
