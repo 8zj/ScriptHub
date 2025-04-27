4244b0bcc8fdbd94ede980b80fe3ac5030d2a6d2
Pick <90311538+8zj@users.noreply.github.com>
2025-04-26 23:16:41 -0400

    Update Data.lua

--- FischData/Data.lua
+++ FischData/Data.lua
@@ -1,11 +1,39 @@
 return {
-    NormalZones = {
-        "Zone 1",
-        "Zone 2",
-        "Zone 3"
-    },
-    EventZones = {
-        "LEGO",
-        "LEGO - Studolodon"
+    Settings = {
+        EventZones = {
+            "LEGO",
+            "LEGO - Studolodon"
+        },
+        Normal = {
+            Farm = {
+                Shark = { 
+                    "Great White Shark", 
+                    "Great Hammerhead Shark", 
+                    "Whale Shark" 
+                },
+                Isonade = { "Isonade" },
+                Scylla = { "Forsaken Veil - Scylla" },
+                Megalodon = { 
+                    "Megalodon Default", 
+                    "Megalodon Ancient", 
+                    "Megalodon Phantom"
+                },
+                Kraken = { 
+                    "The Kraken Pool", 
+                    "Ancient Kraken Pool" 
+                },
+                Whale = { 
+                    "Whales Pool", 
+                    "MobySpawn" 
+                },
+                Orca = { 
+                    "Ancient Orcas Pool", 
+                    "Orcas Pool" 
+                },
+                Serpent = { 
+                    "The Depths - Serpent" 
+                },
+            }
+        }
     }
 }
